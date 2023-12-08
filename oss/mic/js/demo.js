23585a77e3c64b627d83b4d3aeefca0a4fddb260
Peter Day <peterday.main@gmail.com>
2023-12-08 23:03:11 +0000

    move

---
+++ oss/mic/js/demo.js
@@ -0,0 +1,30 @@
+document.querySelector('button').onclick = function(e) {
+    mic.drop(e,[
+        {
+            name: 'Light mode',
+            run: function(){
+                document.body.classList.add('light');
+            }
+        },
+        {
+            name: 'Dark mode',
+            run: function(){
+                document.body.classList.remove('light');
+            }
+        },
+        {
+            name: 'A song about a leopard',
+            run: function(){
+                window.open('https://www.youtube.com/watch?v=5es5MQX0VVA','_blank');
+            }
+        },                
+        {
+            name: 'Destroy the world',
+            class: 'red',
+            run: function(){
+                // run logout
+                alert('kerboom');
+            }
+        }
+    ]);
+};
